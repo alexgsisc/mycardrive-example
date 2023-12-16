fc6b4e51e674ca58434532e3b32c34e2a484d175
Alexis Guadarrama <alexgsisc@gmail.com>
2023-12-16 13:18:37 -0600

    I have finished course of custom data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,15 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto) // use global
 export class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -25,7 +25,8 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  //@UseInterceptors(new SerializeInterceptor(UserDto))
+  //@Serialize(UserDto) by service
   @Get('/:id')
   findUser(@Param('id') id: number) {
     return this.userService.findOne(id);
